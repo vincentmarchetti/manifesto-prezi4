dd9f02a294a26e952bb50b36461a927ad4fca367
Vincent Marchetti <vmarchetti@kshell.com>
2026-03-06 13:02:07 -0500

    commented out coerce_to_resource code
    rewrote cast_to_array and cast_to_resource so that informative error msgs
    are generated if cast_to* is not allowed, these are available for debugging
    in later development

--- src/IResource.ts
+++ src/IResource.ts
@@ -19,17 +19,31 @@ export interface IResource{
  
  export class ResourceOps{
     static cast_to_resource(a : unknown ) : IResource | null {
-        if ( a == null ||
-             Array.isArray(a) ||
-             typeof a != 'object' ||
-             typeof a['type'] !== 'string' ) {
-                const msg = `ResourceOps.cast_to_resource ${a} ${a==null} ${Array.isArray(a)} ${typeof a}`;
+        try{
+            if (Array.isArray(a)){
+                if (a.length == 0) throw new Error("empty array");
+                if (a.length == 1) a = a[0];
+            }
+
+            if (typeof a != 'object')   throw new Error('not an object');
+            if ( a == null )            throw new Error('is null');
+            if ( Array.isArray(a))      throw new Error('is an multi item Array');
+            if ( typeof a['type'] != 'string') {
+                const msg = `type property is ${typeof a['type']}`;
                 throw new Error(msg);
-                return null;
-             }
-        return a as IResource;        
+            }
+            return a as IResource;
+        }
+        catch (error){
+            // @ts-ignore  unused msg
+            const msg = `ResourceOps.cast_to_resource | ${error}`;
+            return null;
+        }  
     }
-             
+    
+    /*  Developer note 3/6/2026: The coerce_to_resource needs some better
+    design. Remove as cruft if this is still here 4/6/2026
+    
     static coerce_to_resource( a:unknown , coerce_array = true ) : IResource | null {
         if ( coerce_array && Array.isArray(a)){
             a = ( (x:unknown[] ):unknown  => {               
@@ -37,32 +51,49 @@ export interface IResource{
                 if (n==0)
                     return null;
                 if (n > 1){
-                    const msg = `IResourceOps.coerce_to_resource : multi item array`;
-                    console.warn(msg);
+                    const msg = `ResourceOps.coerce_to_resource : multi item array`;
+                    return null;
                 }
                 return x[0];                
             })(a as unknown[] );
         }
         return ResourceOps.cast_to_resource(a);
     }
+    */
     
-    static cast_to_array( a: unknown ) : IResource[] {
-        if (a == null) return ([] as IResource[] );
-    
-        const as_resource : IResource | null = ResourceOps.cast_to_resource(a);
-        if (as_resource !== null){
-            return [ as_resource as IResource ];
-        }
-        if (Array.isArray(a)){
-            return a.map( ( t:unknown, index:number):IResource => {
-                const as_resource:IResource | null = ResourceOps.cast_to_resource(t);
-                if (as_resource == null ){
-                    const msg = `ResourceOps.cast_to_array | element {index} not an IResource`;
+    static cast_to_array( a: unknown ) : IResource[] | null {
+        try{
+            if (a == null) return ([] as IResource[] );
+            if (!Array.isArray(a)) {
+                const tmp : IResource | null = ResourceOps.cast_to_resource(a);
+                if (tmp == null){
+                    const msg = `not Array nor IResource`;
                     throw new Error(msg);
                 }
-                return as_resource as IResource;
+                return [tmp] as IResource[];
+            }
+            
+            // at this point a is an Array
+            return a.map( ( t:unknown, index:number):IResource => {
+                try{
+                    if (Array.isArray(t)) throw new Error("is Array");
+                    const as_resource:IResource | null = ResourceOps.cast_to_resource(t);
+                    if (as_resource == null ){
+                        const msg = `not an IResource`;
+                        throw new Error(msg);
+                    }
+                    return as_resource as IResource;
+                }
+                catch (error){                    
+                    const msg = `map element ${index} | ${error}`;  
+                    throw new Error(msg);
+                }
             });
         }
-        return  ([] as IResource[] );
+        catch (error){
+            // @ts-ignore  unused msg
+            const msg = `ResourceOps.cast_to_array | ${error}`;
+            return null;
+        }
     }
  }
