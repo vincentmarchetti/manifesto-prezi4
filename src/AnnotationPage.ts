8e8b1b70cc48260608c132c56efbaf24874cf353
Vincent Marchetti <vmarchetti@kshell.com>
2026-03-07 16:29:07 -0500

    re-implement Items

--- src/AnnotationPage.ts
+++ src/AnnotationPage.ts
@@ -1,24 +1,42 @@
-import { Annotation, IManifestoOptions, ManifestResource } from "./internal";
+import {    Annotation, 
+            IManifestoOptions, 
+            JSONLDResource,
+            ManifestResource,
+            IResource,
+            ResourceOps } from "./internal";
 
 export class AnnotationPage extends ManifestResource {
-  constructor(jsonld: any, options: IManifestoOptions) {
+  constructor(jsonld: IResource, options: IManifestoOptions) {
     super(jsonld, options);
   }
 
-  getItems(): any[] {
-    return this.getProperty("items");
-  }
   
-  getAnnotations():Annotation[] {
-    const items:any[] = this.getProperty("items") ?? [];
-    return items.reduce( (accum, item:any) => {
-        const item_type = item["type"] ?? "";
-        if (item_type === "Annotation")
-            accum.push( new Annotation(item, this.options));
-        else
-            console.warn("AnnotationPage.getAnnotations unrecognized item_type ${item_type}");
-        return accum;
-    },
-    []);
+  
+  get Items() : Annotation[] {
+    try{
+        const itemsProp : unknown = this.ResourceProperty("items");
+        const resourceItems:IResource[] | null = ResourceOps.cast_to_array( itemsProp );
+        
+        if (resourceItems == null ){
+            const msg = `invalid value`;
+            throw new Error(msg);
+        }
+        return resourceItems.map( (item:IResource, index:number):Annotation => {
+            try{
+                const resource:JSONLDResource = JSONLDResource.Construct( item, this.options);
+                if (!["Annotation"].includes( resource.ResourceType)) 
+                    throw new Error("not Annotation");
+                return resource as Annotation;
+            }
+            catch (error){
+                const msg = 'map at element ${index} | ${error}';
+                throw new Error(msg);
+            }
+        });
+    }
+    catch (error){
+        const msg = `AnnotationPage.Items | ${error}`;
+        throw new Error(msg);
+    }
   }
 }
