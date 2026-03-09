3148f846fcd5a3e8b07d6a46ceee06e7b7f57866
Vincent Marchetti <vmarchetti@kshell.com>
2026-03-09 15:57:23 -0400

    Fixed logic error in the ResourceHasPropery method

--- src/JSONLDResource.ts
+++ src/JSONLDResource.ts
@@ -21,7 +21,7 @@ export class JSONLDResource {
   }
   
   ResourceHasProperty( name: string ) : boolean {
-    return Boolean( this.ResourceProperty(name) == null);
+    return Boolean( this.ResourceProperty(name) != null);
   }
 
   get ResourceType():string {
