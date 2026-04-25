c3d30c60e657708994429054c2e0786c5dbcb77d
Vincent Marchetti <vmarchetti@kshell.com>
2026-04-25 15:43:06 -0400

    added testing for finding annotations associated with an embedded Canvas or embedded Scene

--- test/annotation_search.test.js
+++ test/annotation_search.test.js
@@ -6,7 +6,7 @@ import * as fs from "node:fs";
 let manifest_json  = null;
  
 before(function(){
-    const manifest_path = './test/fixtures/prezi4-examples/uc06_multiple_3d_objects.json';
+    const manifest_path = './test/fixtures/prezi4-examples/uc06_canvas_in_scene.json';
     manifest_json = JSON.parse( fs.readFileSync(manifest_path, 'utf8'));
 });
                
@@ -18,13 +18,22 @@ describe('seach_annotation', function() {
         expect(null_result).to.equal(null);
     });
 
-    it('null search', function() {
+    it('annotation search', function() {
         expect(manifest_json).to.exist; 
-        const anno_id = "https://iiif.io/api/presentation/4.0/example/uc06/3d/anno2";
+        const anno_id = "https://iiif.io/api/presentation/4.0/example/uc06/scene/canvas-scene/anno/1";
         const found = find_annotation_in_manifest(anno_id , manifest_json);
         expect(found).to.exist;
         expect(found["type"]).to.equal("Annotation");
     });
+    
+    it('canvas in scene annotation search', function() {
+        expect(manifest_json).to.exist; 
+        const anno_id = "https://iiif.io/api/presentation/4.0/example/uc06/canvas/chessboard/anno/1";
+        const found = find_annotation_in_manifest(anno_id , manifest_json);
+        expect(found).to.exist;
+        expect(found["type"]).to.equal("Annotation");
+    });
+
         
     
 });
